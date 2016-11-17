7a02a2d1f2690c8aeaf57372ee6e092d13676f36
Michał Szewczuk <michal.szewczuk.ext@nokia.com>
2016-11-17 15:46:30 +0100

    add 04-list/list-ex3 (forward_list, iterators)

---
+++ 04-list/list-ex3.cpp
@@ -0,0 +1,23 @@
+#include <forward_list>
+#include <iostream>
+#include <algorithm>
+
+int
+main()
+{
+  std::forward_list<int> list{5,4,3,7,6,8,9,0,1,2};
+
+  auto first = list.begin();
+  auto last = list.end();
+
+  std::cout << "Size: " << std::distance(first, last) << std::endl;
+
+  auto fifth = list.begin();
+  std::advance(fifth, 4);
+
+  std::cout << "Fifth: " << *fifth << std::endl;
+
+  std::cout << "Distance: " << std::distance(first, fifth) << std::endl;
+
+  return 0;
+}
